e8c8293a6caf9da4a562319cbec45b259163cf41
shoheiyamamoto <provisioninc@outlook.jp>
2022-05-12 17:34:33 +0900

    create details.html.erb

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,42 +8,7 @@ module Admin
       @female = User.where(sex: 0)
     end
 
-    def new
-      @user = User.new
-    end
-
-    def show; end
-
-    def create
-      @user = User.new(user_params)
-      if @user.save
-        redirect_to admin_users_index_path, notice: "ユーザー「#{@user.name}」を登録しました。"
-      else
-        render :new
-      end
-    end
-
-    def edit; end
-
-    def update
-      if @user.update(user_params)
-        get_point
-        redirect_to admin_users_index_path, notice: "ユーザー「#{@user.name}」を更新しました。"
-      else
-        render :edit
-      end
-    end
-
-    def destroy
-      @user.destroy
-      redirect_to admin_users_index_path, notice: "ユーザー「#{@user.name}」を削除しました。"
-    end
-
     private
-
-    def user_params
-      params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation, :sex, :character, :hobby, :generation)
-    end
 
     def require_admin
       redirect_to root_path unless current_user.admin?
